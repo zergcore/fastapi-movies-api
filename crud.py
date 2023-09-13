8d7a35f147b0de4dcb69adf75b443d148038fb37
Zaidibeth Ramos <zaidi.ramos@gmail.com>
2023-09-13 10:06:08 -0400

    refactoring

--- crud.py
+++ crud.py
@@ -1,35 +1,39 @@
-from sqlalchemy.orm import Session
+# from sqlalchemy.orm import Session
+from config.database import SessionLocal
 
-import database, models
-
-
-def get_user(db: Session, user_id: int):
-    return db.query(database.user.User).filter(database.user.User.id == user_id).first()
-
-
-def get_user_by_email(db: Session, email: str):
-    return db.query(database.user.User).filter(database.user.User.email == email).first()
+from database.movie import Movie
+from database.user import User
+from models.movie import Movie as MovieModel
+from models.user import User as UserModel, UserCreate
 
 
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(database.user.User).offset(skip).limit(limit).all()
+def get_user(db: SessionLocal, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
 
 
-def create_user(db: Session, user: models.user.UserCreate):
+def get_user_by_email(db: SessionLocal, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+
+def get_users(db: SessionLocal, skip: int = 0, limit: int = 100):
+    return db.query(User).offset(skip).limit(limit).all()
+
+
+def create_user(db: SessionLocal, user: UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
-    db_user = database.User(email=user.email, password=fake_hashed_password)
+    db_user = User(email=user.email, password=fake_hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
 
-def get_movies(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(database.movie.Movie).offset(skip).limit(limit).all()
+def get_movies(db: SessionLocal, skip: int = 0, limit: int = 100):
+    return db.query(Movie).offset(skip).limit(limit).all()
 
 
-def create_movie(db: Session, movie: models.movie.Movie):
-    db_movie = database.movie.Movie(**movie.dict())
+def create_movie(db: SessionLocal, movie: Movie):
+    db_movie = Movie(**movie.dict())
     db.add(db_movie)
     db.commit()
     db.refresh(db_movie)
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI, Body, Path, Query, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
-# from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-# from pydantic import BaseModel
-# from models.movie import Movie as MovieModel
 from sqlalchemy.orm import Session
 from typing import List
-
 import json
+
 from models.movie import Movie as MovieModel
 from models.user import User as UserModel, UserCreate
 from crud import get_movies, get_user, get_user_by_email, get_users
@@ -20,7 +17,6 @@ app = FastAPI()
 app.title = "Movies API"
 app.version = "0.0.1"
 app.description = "API to get movies"
-# app.terms_of_service = "None yet"
 app.contact = {"name": "Zaidibeth Ramos", "email": "zergcoredev@gmail.com"}
 app.license_info = {"name": "MIT", "url": "https://opensource.org/licenses/MIT"}
 app.openapi_tags = [{"name": "Home", "description": "Home page"}]
@@ -29,7 +25,7 @@ Base.metadata.create_all(bind=engine)
 
 # Dependency
 def get_db():
-    db = Session()
+    db = SessionLocal()
     try:
         yield db
     finally:
@@ -45,11 +41,13 @@ with open("movies.json") as f:
 def message():
     return JSONResponse(content={"message": "Hello World"}, status_code=200)
 
+
 @app.post('/login', tags=['auth'])
 def login(user: UserCreate):
     if user.email == "admin@gmail.com" and user.password == "admin":
         token: str = create_token(user.model_dump())
         return JSONResponse(status_code=200, content=token)
+
 
 @app.post("/users/", tags=["Users"], response_model=UserModel)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
@@ -72,11 +70,10 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
+
 @app.get("/movies", tags=["Movies"], response_model=List[MovieModel], status_code=200)
-def index():
-    # return JSONResponse(content=movies, status_code=200)
-    db = SessionLocal()
-    result = db.query(Movie).all()
+def read_movies(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    result = get_movies(db=db, skip=skip, limit=limit)
     return JSONResponse(content=jsonable_encoder(result), status_code=200)
 
 
@@ -88,11 +85,6 @@ def index(id: int = Path(gt=0)):
         return JSONResponse(content=jsonable_encoder(result), status_code=200)
     else:
         return JSONResponse(content={"message": "Movie not found"}, status_code=404)
-    # for item in movies:
-    #     if item["id"] == id:
-    #         return JSONResponse(content=item, status_code=200)
-    # else:
-    #     return JSONResponse(content=[], status_code=404)
 
 
 @app.get("/movies/", tags=["Movies"], response_model=List[MovieModel], status_code=200)
@@ -103,37 +95,15 @@ def index(category: str = Query(min_length=3, max_length=20)):
         return JSONResponse(content=jsonable_encoder(result), status_code=200)
     else:
         return JSONResponse(content={"message": "Category not found"}, status_code=404)
-    # data = [movie for movie in movies if movie["category"].lower() == category.lower()]
-    # if len(data) == 0:
-    #     return JSONResponse(content=[], status_code=404)
-    # return JSONResponse(content=data, status_code=200)
 
 
 @app.post("/movies", tags=["Movies"], response_model=dict, status_code=201)
-def index(movie: MovieModel):
-    db = SessionLocal()
-    new_movie = Movie(**movie.model_dump())
-    # movies.append(movie)
-    db.add(new_movie)
-    db.commit()
+def create_movie(
+    movie: MovieModel, db: SessionLocal = Depends(get_db)
+):
+    create_movie(db=db, movie=movie)
     return JSONResponse(content={"message": "Movie added successfully"}, status_code=201)
 
-
-# @app.post("/movies", tags=["Movies"], response_model=list[MovieModel])
-# def index(id: int = Body(...), title: str = Body(...), year: int = Body(...), director: str = Body(...), duration: str = Body(...), genre: str = Body(...), rating: float = Body(...), votes: int = Body(...), budget: int = Body(...), revenue: int = Body(...), category: str = Body(...)):
-# movie = {
-#     "id": id,
-#     "title": title,
-#     "year": year,
-#     "director": director,
-#     "duration": duration,
-#     "genre": genre,
-#     "rating": rating,
-#     "votes": votes,
-#     "budget": budget,
-#     "revenue": revenue,
-#     "category": category,
-# }
 
 @app.put("/movies/{id}", tags=["Movies"], response_model=dict, status_code=200)
 def update_movie(id: int, movie: MovieModel):
@@ -154,22 +124,6 @@ def update_movie(id: int, movie: MovieModel):
         result.year = movie.year
         db.commit()
         return JSONResponse(content={"message": "Movie updated successfully"}, status_code=201)
-    
-    # for item in movies:
-    #     if item["id"] == id:
-    #         item["title"] = movie.title
-    #         item["year"] = movie.year
-    #         item["director"] = movie.director
-    #         item["duration"] = movie.duration
-    #         item["genre"] = movie.genre
-    #         item["votes"] = movie.votes
-    #         item["budget"] = movie.budget
-    #         item["revenue"] = movie.revenue
-    #         item["rating"] = movie.rating
-    #         item["category"] = movie.category
-    #         JSONResponse(content = {message: "Movie updated successfully"}, status_code = 200)
-    # else:
-    #     return JSONResponse(content = {message: "Movie not found"}, status_code = 404)
 
 
 @app.delete("/movies/{id}", tags=["Movies"], response_model=dict, status_code=200)
@@ -182,22 +136,4 @@ def delete_movie(id: int):
         db.delete(result)
         db.commit()
         return JSONResponse(content={"message": "Movie deleted successfully"}, status_code=200)
-    # for item in movies:
-    #     if item["id"] == id:
-    #         movies.remove(item)
-    #         return JSONResponse(content = {message: "Movie deleted successfully"}, status_code = 200)
-    # else:
-    #     return JSONResponse(content = {message: "Movie not found"}, status_code = 404)
-    
 
-# @app.post("/movies/", response_model=MovieModel)
-# def create_movie(
-#     movie: MovieModel, db: Session = Depends(get_db)
-# ):
-#     return create_movie(db=db, movie=movie)
-
-
-# @app.get("/movie/", response_model=list[MovieModel])
-# def read_movies(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     movies = get_movies(db, skip=skip, limit=limit)
-#     return movies
